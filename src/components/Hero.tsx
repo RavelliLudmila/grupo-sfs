d0452416e03eea89956b0d6c7f6ff04e508fd199
Ludmila <lud.ravelli@gmail.com>
2026-02-12 12:25:24 -0300

    Add Hero component with background image and call-to-action button
    - Created a new Hero component that features a background image and a title.
    - Included descriptive text to highlight the benefits of the software.
    - Added a button that links to the presentation page.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./ui/button";
+
+export default function Hero() {
+    return(
+        <>
+        <section style={{backgroundImage: "url('hero-bg.png')"}}>
+            <div className="container bg-linear-to-r from-primary to-black/50 mx-auto px-6 py-20 text-start">
+                <h1 className="text-4xl font-bold text-background md:text-5xl lg:text-6xl">
+                    Software médico de vanguardia
+                </h1>
+                <p className="mt-5 text-lg text-background md:text-xl lg:text-2xl">
+                    Maximice la eficiencia de su institución a través de nuestras soluciones informáticas.
+                </p>
+                <p className="mt-5 text-lg text-background md:text-xl lg:text-2xl">
+                    Simplifique la gestión de sus servicios, y optimice la atención a sus pacientes.
+                </p>
+                <Button variant="outline" size="lg" className="mt-5" onClick={() => window.open('https://www.sfs.com.ar/presentacion', '_blank')}>
+                    Presentación
+                </Button>
+            </div>
+        </section>
+        </>
+    )
+}
