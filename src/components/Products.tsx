68b2b1fbf8e077fa21d15a338acbe7323b1cd6c8
Ludmila <lud.ravelli@gmail.com>
2026-02-12 16:05:57 -0300

    feat: add patient products section and toggle functionality in Products component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import { Button } from './ui/button';
 import { ButtonGroup } from './ui/button-group';
 import { Card, CardDescription, CardHeader, CardTitle } from './ui/card';
 import { ArrowRight } from 'lucide-react';
 
 export default function Products() {
-    const products = [
+    const [showInstitutions, setShowInstitutions] = useState(true);
+    
+    const productsInstitutions = [
         {
             image: '/images/mockup-healthcare-ai.webp',
             name: 'HealthCareAI',
@@ -32,37 +37,70 @@ export default function Products() {
             link: 'https://www.sfs.com.ar/sapma',
         },
     ];
+    const productsPatients = [
+        {
+            image: '/images/mockup-app.webp',
+            name: 'App para el paciente',
+            description: 'Aplicación móvil para facilitar el acceso de los pacientes a su información clínica.',
+        },
+        {
+            image: '/images/mockup-teleconsulta.webp',
+            name: 'Teleconsulta',
+            description: 'Consultas Paciente-Profesional desde la comodidad de su hogar.',
+        },
+        {
+            image: '/images/mockup-triage.webp',
+            name: 'Triage',
+            description: 'Protocolo de selección y clasificación de pacientes.',
+        },
+    ];
+
     return (
         <>
             <section className="container mx-auto px-6 py-20 text-center">
                 <h2 className="text-3xl font-bold text-secondary md:text-4xl lg:text-5xl">PRODUCTOS Y SOLUCIONES DE SOFTWARE</h2>
                 <ButtonGroup>
-                    <Button variant="outline" size="lg" className="mt-5">
+                    <Button variant="outline" size="lg" className="mt-5" onClick={() => setShowInstitutions(true)}>
                         Para instituciones
                     </Button>
-                    <Button variant="outline" size="lg" className="mt-5">
+                    <Button variant="outline" size="lg" className="mt-5" onClick={() => setShowInstitutions(false)}>
                         Para sus pacientes
                     </Button>
                 </ButtonGroup>
                 <div className="mt-10 grid gap-8 grid-cols-4">
-                    {products.map((product) => (
-                        <Card key={product.name} className="relative mx-auto w-full max-w-sm pt-0">
-                            <div className="absolute inset-0 z-30 aspect-video" />
-                            <Image
-                                src={product.image}
-                                alt={product.name}
-                                className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
-                            />
-                            <CardHeader>
-                                <CardTitle>{product.name}</CardTitle>
-                                <CardDescription>{product.description}</CardDescription>
-                                <Button variant="link" size="sm" className="mt-4" onClick={() => window.open(product.link)}>
-                                    Ver más
-                                    <ArrowRight className="ml-2" />
-                                </Button>
-                            </CardHeader>
-                        </Card>
-                    ))}
+                    {showInstitutions
+                        ? productsInstitutions.map((product) => (
+                              <Card key={product.name} className="relative mx-auto w-full max-w-sm pt-0">
+                                  <div className="absolute inset-0 z-30 aspect-video" />
+                                  <Image
+                                      src={product.image}
+                                      alt={product.name}
+                                      className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+                                  />
+                                  <CardHeader>
+                                      <CardTitle>{product.name}</CardTitle>
+                                      <CardDescription>{product.description}</CardDescription>
+                                      <Button variant="link" size="sm" className="mt-4" onClick={() => window.open(product.link)}>
+                                          Ver más
+                                          <ArrowRight className="ml-2" />
+                                      </Button>
+                                  </CardHeader>
+                              </Card>
+                          ))
+                        : productsPatients.map((product) => (
+                              <Card key={product.name} className="relative mx-auto w-full max-w-sm pt-0">
+                                  <div className="absolute inset-0 z-30 aspect-video" />
+                                  <Image
+                                      src={product.image}
+                                      alt={product.name}
+                                      className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+                                  />
+                                  <CardHeader>
+                                      <CardTitle>{product.name}</CardTitle>
+                                      <CardDescription>{product.description}</CardDescription>
+                                  </CardHeader>
+                              </Card>
+                          ))}
                 </div>
                 <h3 className="text-2xl text-secondary">¿Querés tener una experiencia personalizada?</h3>
                 <Button variant="default" size="lg" className="mt-5" onClick={() => window.open('/presentation')}>
