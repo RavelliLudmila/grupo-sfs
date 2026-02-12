94916857650f6c30243ed9bd8d91c75bba429a20
Ludmila <lud.ravelli@gmail.com>
2026-02-12 16:18:02 -0300

    feat: add PoliticaDeCalidad component with quality policy content

---
+++ src/app/politica-de-calidad/page.tsx
@@ -0,0 +1,30 @@
+export default function PoliticaDeCalidad() {
+    return (
+        <section className="container mx-auto px-48 py-24 text-start">
+            <div className="flex flex-col justify-start items-start gap-5 mb-10">
+                <h2 className="text-3xl font-bold text-secondary md:text-4xl lg:text-5xl">Política de calidad</h2>
+                <div className="flex flex-col justify-start items-start gap-5">
+                    <p className="text-lg text-secondary">
+                        En el Grupo SFS brindamos servicios y desarrollo de aplicaciones predominantemente para el sector salud.
+                    </p>
+                    <p className="text-lg text-secondary">
+                        Nuestro objetivo es lograr la satisfacción de nuestros clientes con énfasis en la mejora continua, siendo una empresa que
+                        brinda servicios de excelente calidad e innovación, trabajando con información crítica y sensible y priorizando la
+                        confidencialidad y el criterio profesional en cada proyecto.
+                    </p>
+                    <p className="text-lg text-secondary">
+                        Estos servicios tienden a satisfacer necesidades, solucionar problemas, mejorar procesos y desarrollar ventajas competitivas
+                        de nuestros clientes a través de soluciones tecnológicas sustentables.
+                    </p>
+                    <p className="text-lg text-secondary">
+                        Nos esforzamos día a día para lograr el cumplimiento de estándares de desarrollo, generando productos de alta calidad,
+                        cuidando la información de cada institución y ofreciendo al mercado la mejor combinación de conocimientos y servicios.
+                    </p>
+                    <p className="text-lg text-secondary">
+                        Promovemos la generación de alianzas comerciales para ingresar a los mercados externos y seguir afianzando el mercado local.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
