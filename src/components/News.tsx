03c9d41303d48736803da278c275060ca75774af
Ludmila <lud.ravelli@gmail.com>
2026-02-13 12:17:52 -0300

    feat(News): refactor News component layout and improve carousel display logic

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -22,28 +22,29 @@ export default function News() {
             video: '/images/hero-bg.webp',
         },
     ];
+
+    const displayNews = news.length == 3 ? [...news, ...news] : news;
+
     return (
-        <section className="py-16 bg-gray-50">
-            <div className="container mx-auto px-6">
-                <h2 className="text-3xl font-bold text-secondary text-center mb-12 md:text-4xl lg:text-5xl">NOVEDADES</h2>
-                <Carousel opts={{ align: 'start', loop: true }} className="w-full max-w-7xl mx-auto">
-                    <CarouselContent className="-ml-4">
-                        {news.map((item, index) => (
-                            <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
-                                <Card className="overflow-hidden h-full">
-                                    <Image src={item.video} alt={item.title} width={400} height={300} className="w-full object-cover aspect-video" />
-                                    <CardHeader>
-                                        <CardTitle>{item.title}</CardTitle>
-                                        <CardDescription>{item.description}</CardDescription>
-                                    </CardHeader>
-                                </Card>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious className="left-0" />
-                    <CarouselNext className="right-0" />
-                </Carousel>
-            </div>
+        <section className="container flex flex-col justify-between items-center mx-auto px-6 py-20 text-center">
+            <h2 className="text-3xl font-bold text-secondary pb-8">NOVEDADES</h2>
+            <Carousel opts={{ align: 'start', loop: true }} className="w-full max-w-7xl mx-auto">
+                <CarouselContent className="-ml-4">
+                    {displayNews.map((item, index) => (
+                        <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
+                            <Card className="overflow-hidden h-full pt-0 text-start">
+                                <Image src={item.video} alt={item.title} width={400} height={300} className="w-full object-cover aspect-video" />
+                                <CardHeader>
+                                    <CardTitle>{item.title}</CardTitle>
+                                    <CardDescription>{item.description}</CardDescription>
+                                </CardHeader>
+                            </Card>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </section>
     );
 }
